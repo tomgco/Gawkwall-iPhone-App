1eb616903cb2afe06cc87792a6c230682e950c16
tomgallacher <tomgallacher23@gmail.com>
2011-03-03 01:10:04 +0000

    Fixed Mismatch of Key/Values

--- Classes/LoginViewController.m
+++ Classes/LoginViewController.m
@@ -24,7 +24,7 @@
 	httpRequest  = [ASIFormDataRequest requestWithURL:[NSURL URLWithString:GAWK_API_LOCAITON]];
 	[httpRequest setPostValue:email forKey:@"EmailAddress"];
 	[httpRequest setPostValue:gawkPassword forKey:@"Password"];
-	[httpRequest setPostValue:@"Action" forKey:@"Member.Login"];
+	[httpRequest setPostValue:@"Member.Login" forKey:@"Action"];
 	[httpRequest setTimeOutSeconds:20];	
 	[httpRequest setDelegate:self];
 	[httpRequest setDidFailSelector:@selector(uploadFailed:)];
