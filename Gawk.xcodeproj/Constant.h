5a464d35e67d14d89d7a1ead6dc5607432b6aa5b
tomgallacher <tomgallacher23@gmail.com>
2011-03-03 00:09:36 +0000

    Adding Constants for Api, and updating main view

---
+++ Gawk.xcodeproj/Constant.h
@@ -0,0 +1,9 @@
+//
+//  Constant.h
+//  Gawk
+//
+//  Created by Tom Gallacher on 02/03/2011.
+//  Copyright 2011 Clock Ltd. All rights reserved.
+//
+
+#define GAWK_API_LOCAITON @"http://testing.gawkwall.com/api/"
