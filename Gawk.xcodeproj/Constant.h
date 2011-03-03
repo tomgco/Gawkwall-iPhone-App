80342a56061f68d2a98616b79de2654c14e4cb81
tomgallacher <tomgallacher23@gmail.com>
2011-03-03 01:47:38 +0000

    Adding Public and Private Keys

--- Gawk.xcodeproj/Constant.h
+++ Gawk.xcodeproj/Constant.h
@@ -7,3 +7,5 @@
 //
 
 #define GAWK_API_LOCAITON @"http://testing.gawkwall.com/api/"
+#define GAWK_API_PUBKEY @"787a4aa2778d73f6f2da1598e13668753b5a8010"
+#define GAWK_API_PRIVKEY @"d20533231c09074a07de1cc9f593c1765bdcf146f7efee55abe61e66a2cda80b"
