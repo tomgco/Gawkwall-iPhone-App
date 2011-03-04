85ac509de8d9bc686eaf29f62634c01c1073210d
tomgallacher <tomgallacher23@gmail.com>
2011-03-04 01:17:33 +0000

    Adding Facebook Tokens and renaming ID to APP_ID

--- Gawk.xcodeproj/Constant.h
+++ Gawk.xcodeproj/Constant.h
@@ -9,5 +9,9 @@
 #define GAWK_API_LOCAITON @"http://testing.gawkwall.com/api/"
 #define GAWK_API_PUBKEY @"787a4aa2778d73f6f2da1598e13668753b5a8010"
 #define GAWK_API_PRIVKEY @"d20533231c09074a07de1cc9f593c1765bdcf146f7efee55abe61e66a2cda80b"
-#define GAWK_FACEBOOK_ID @"148522008529839"
+#define GAWK_FACEBOOK_APP_ID @"148522008529839"
 #define GAWK_FACEBOOK_FB_URL @"fb148522008529839"
+
+
+#define FB_ACCESS_TOKEN_KEY @"fb_access_token"
+#define FB_EXPIRATION_DATE_KEY @"fb_expiration_date"
