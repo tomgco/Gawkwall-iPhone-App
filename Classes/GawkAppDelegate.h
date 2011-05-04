5c27adb7b500241e44438800a1637788ff4f07a0
tomgallacher <tomgallacher23@gmail.com>
2011-05-04 20:19:47 +0100

    Committing missing header file

--- Classes/GawkAppDelegate.h
+++ Classes/GawkAppDelegate.h
@@ -7,22 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "LoginViewController.h"
-#import "LoginModel.h"
 
 @class CameraViewController;
 
 @interface GawkAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
   UIWindow	*window;
   UITabBarController	*tabBarController;
-	LoginViewController *loginView;
 	CameraViewController *cameraViewController;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
 @property (nonatomic, retain) IBOutlet CameraViewController *cameraViewController;
-@property (readonly) LoginViewController *loginView;
 
 -(void)showLoginView:(BOOL)animated;
 -(IBAction)logout;
